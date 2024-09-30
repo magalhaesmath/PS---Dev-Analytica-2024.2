e9f6fb7c5cd6102ac0d737bc1b476b798ac3f18e
magalhaesmath <152709940+magalhaesmath@users.noreply.github.com>
2024-09-30 14:52:56 -0300

    q3p2.py
    Questão 3 da Tarefa de Programação 2, PS - Dev UFRJ Analytica 2024.2

--- q3p2.py
+++ q3p2.py
@@ -26,35 +26,17 @@ def main():
     return
   if valor == 0:
     print("0 nota(s)")
-  
-  resto = valor
-  notas100 = resto // 100
-  resto = resto % 100
-  notas50 = resto // 50
-  resto = resto % 50
-  notas20 = resto // 20
-  resto = resto % 20
-  notas10 = resto // 10
-  resto = resto % 10 
-  notas5 = resto // 5
-  resto = resto % 5
-  notas2 = resto // 2
-  resto = resto % 2
-  moedas1 = resto // 1
-  resto = resto % 1
-  moedas50cent = resto // 0.5
-  resto = resto % 0.5
-  moedas25cent = resto // 0.25
-  resto = resto % 0.25
-  moedas10cent = resto // 0.1
-  resto = resto % 0.1
-  moedas5cent = resto // 0.05
-  resto = resto % 0.05
-  moedas1cent = resto // 0.01
-  resto = resto % 0.01
 
-  print(f"NOTAS:\n{notas100} nota(s) de R$ 100.00\n{notas50} nota(s) de R$ 50.00\n{notas20} nota(s) de R$ 20.00\n{notas10} nota(s) de R$ 10.00\n{notas5} nota(s) de R$ 5.00\n{notas2} nota(s) de R$ 2.00\n")
-  print(f"MOEDAS:\n{moedas1} moeda(s) de R$ 1.00\n{moedas50cent} moeda(s) de R$ 0.50\n{moedas25cent} moeda(s) de R$ 0.25\n{moedas10cent} moeda(s) de R$ 0.10\n{moedas5cent} moeda(s) de R$ 0.05\n{moedas1cent} moeda(s) de R$ 0.01\n")
+  valores = [100, 50, 20, 10, 5, 2, 1, .5, .25, .1, .05, .01]
+  notas = [notas100, notas50, notas20, notas10, notas5, notas2, moedas1, moedas50cent, moedas25cent, moedas10cent, moedas5cent, moedas1cent]
+
+  resto = valor
+  for i in range(len(valores)):
+    notas[i] = int(resto // valores[i])
+    resto = resto % valores[i]
+
+  print(f"NOTAS:\n{notas[0]} nota(s) de R$ 100.00\n{notas[1]} nota(s) de R$ 50.00\n{notas[2]} nota(s) de R$ 20.00\n{notas[3]} nota(s) de R$ 10.00\n{notas[4]} nota(s) de R$ 5.00\n{notas[5]} nota(s) de R$ 2.00\n")
+  print(f"MOEDAS:\n{notas[6]} moeda(s) de R$ 1.00\n{notas[7]} moeda(s) de R$ 0.50\n{notas[8]} moeda(s) de R$ 0.25\n{notas[9]} moeda(s) de R$ 0.10\n{notas[10]} moeda(s) de R$ 0.05\n{notas[11]} moeda(s) de R$ 0.01\n")
 
 if __name__ == "__main__":
   main()
